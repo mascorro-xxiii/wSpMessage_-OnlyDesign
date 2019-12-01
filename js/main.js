eec718307126062f113950a2139da2fc8836afe1
Angel Mascorro <amascorro00@gmail.com>
2019-11-30 20:09:48 -0600

    Final Design (vol 0.0.1)

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function abrirLink(){
+	var numero = document.getElementById("numero").value;
+			// 33 + 8 numbers, for GDL City
+			// the number 33 could be changed
+            window.open('https://wa.me/33' + numero);
+}
